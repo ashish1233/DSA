8aa2e6825879c36b5989abb9d8554028882d6f35
Ashish Varyani <ashuvaryani123@gmail.com>
2024-12-23 20:36:10 +0530

    Time: 0 ms (100.00%) | Memory: 59.7 MB (12.02%) - LeetSync

---
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var insertIntoBST = function(root, val) {
+    //let result = []
+     let current = root
+    function helper(current, prev, val){   
+        if(!current){
+            if(!prev){
+                let root = new TreeNode(val)
+                return root
+            }
+            if(prev.val > val){
+                prev.left = new TreeNode(val)
+            }
+            else{
+                prev.right = new TreeNode(val)
+            }
+            return root
+        }
+      
+        if(val < current.val ){
+            return helper(current.left, current, val)
+        }
+        else if(val > current.val ){
+           return helper(current.right, current, val)
+        }
+    }
+   return helper(current, null, val)
+};
