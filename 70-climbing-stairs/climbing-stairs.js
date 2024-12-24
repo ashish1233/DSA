351d2d5f8f9b074b1539bcfe2880ac8f62b1cfa6
Ashish Varyani <ashuvaryani123@gmail.com>
2024-12-24 15:22:06 +0530

    Time: 0 ms (100.00%) | Memory: 48.9 MB (51.85%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    let memo ={}
+
+    function helper(n){
+         if (n === 1) return 1;
+    if (n === 2) return 2;
+    
+       if (memo[n] !== undefined)
+       { 
+        return memo[n]; 
+       
+       }
+    
+        memo[n] = helper(n-1) + helper(n-2)
+
+        return  memo[n]
+    }
+    
+    return helper(n)
+};
