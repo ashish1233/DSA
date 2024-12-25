86b48a501f0dc82c992f2c47645031d0f6550d4c
Ashish Varyani <ashuvaryani123@gmail.com>
2024-12-25 17:32:40 +0530

    Time: 0 ms (100.00%) | Memory: 53 MB (92.10%) - LeetSync

---
+++ 55-jump-game/jump-game.js
@@ -0,0 +1,52 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+     let max =0
+	for(let i=0; i< nums.length; i++){
+        if(i> max){
+			return false
+		}
+		let num = i+ nums[i]
+        max = Math.max(max, num)
+
+        if(max >= nums.length-1){
+
+ 			return true
+		}
+                        
+	}
+       return false
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   let j = nums.length-1
+//      for(let i=nums.length-2; i>=0; i-- ){
+//          if(nums[i]>= j-i){
+//             j= i 
+//          }
+//          console.log(j)
+//      }
+//      if(j==0){
+//              return true
+//          }
+//          else{
+//              return false
+//          }
