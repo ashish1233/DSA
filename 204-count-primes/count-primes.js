3af5fc82af36f66a5b2b574f65a72e771b76ba0f
Ashish Varyani <ashuvaryani123@gmail.com>
2024-12-28 02:12:38 +0530

    Time: 1184 ms (12.33%) | Memory: 49.1 MB (98.83%) - LeetSync

--- 204-count-primes/count-primes.js
+++ 204-count-primes/count-primes.js
@@ -8,15 +8,15 @@ var countPrimes = function(n) {
         return 0
     }
     function isPrime(num){
-        if (num <= 1) return false; // 0 and 1 are not prime numbers
-    if (num <= 3) return true; // 2 and 3 are prime numbers
-    if (num % 2 === 0 || num % 3 === 0) return false;
+        //if (num <= 1) return false; // 0 and 1 are not prime numbers
+         if (num <= 3) return true; // 2 and 3 are prime numbers
+          if (num % 2 === 0 || num % 3 === 0) return false;
 
     // Check divisors up to the square root of the number
-    for (let i = 5; i * i <= num; i += 6) {
-        if (num % i === 0 || num % (i + 2) === 0) return false;
-    }
-    return true;
+            for (let i = 5; i * i <= num; i += 6) {
+                if (num % i === 0 || num % (i + 2) === 0) return false;
+            }
+            return true;
     }
     for (let i = 2; i < n; i++) {
         if (isPrime(i)) { 
