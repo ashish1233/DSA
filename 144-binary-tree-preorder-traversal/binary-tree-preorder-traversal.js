24113bfb8d2e5b746c02d53257a397e0e8282aa8
Ashish Varyani <ashuvaryani123@gmail.com>
2024-12-30 22:45:09 +0530

    Time: 0 ms (100.00%) | Memory: 49.2 MB (54.19%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.js
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.js
@@ -10,50 +10,23 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-// var preorderTraversal = function(root) {
-//         let curr = root
-//     var preorder = []
-//     while(curr !=null){
-//          if(curr.left == null){
-//             preorder.push(curr.val)
-//             curr = curr.right 
-//          }
-//          else{
-//             let prev = curr.left
-//             while(prev.right && prev.right != curr){
-//                 prev = prev.right
-//             }
-//             if(prev.right == null ){
-//                 prev.right = curr
-//                 preorder.push(curr.val)
-//                 curr = curr.left
-//             }
-//             else{
-//                 prev.right = null
-//                 curr= curr.right
-//             }
-
-//          }
-//     }
-//   return preorder
-// };
-
-//Recursive
 var preorderTraversal = function(root) {
-         let result = []
-         function preOrder(root){
-                 if(root != null) {
-                     result.push(root.val)
-                     if(root.left!=null){
-                        preOrder(root.left)
-                     }
-                     if(root.right!=null){
-                          preOrder(root.right)
-                     }
-
-                 }
+    
+    let result = []
+    function helper(root){
+         if(!root){
+            return
          }
+          result.push(root.val)
+          if(root.left){
+             helper(root.left)
+          }
+          if(root.right){
+             helper(root.right)
+          }
+    }
 
-         preOrder(root)
-         return result
+    helper(root)
+    return result
+
 };
