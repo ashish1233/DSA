b57f2dc5f31fbbf97545c110e41b3746407cf55b
Ashish Varyani <ashuvaryani123@gmail.com>
2024-12-30 12:39:29 +0530

    Time: 0 ms (100.00%) | Memory: 49 MB (67.94%) - LeetSync

--- 162-find-peak-element/find-peak-element.js
+++ 162-find-peak-element/find-peak-element.js
@@ -3,27 +3,45 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-    if(nums.length == 1){
-        return 0
-    }
-    if(nums.length == 2){
-        if(nums[0]> nums[1]){
-            return 0
+    let left = 0;
+    let right = nums.length - 1;
+
+    while(left<right){
+         let mid = Math.floor((left + right) / 2);
+         if (nums[mid] < nums[mid + 1]) {
+            left = mid + 1; // Peak is in the right half
         }
-        else{
-            return 1
+        else {
+            right = mid; // Peak is in the left half (or mid is a peak)
         }
+        
     }
-    if(nums[0] > nums[1]){
-        return 0
-    }
-    else if(nums[nums.length-1]> nums[nums.length-2]){
-        return nums.length-1
-    }
-    for(let i = 1; i<nums.length ; i++){
-        if(nums[i] > nums[i-1] && nums[i]> nums[i+1]){
-            return i
-        }
-    }
-    return -1
+    return left
 };
+
+
+
+// o(n)
+// if(nums.length == 1){
+//         return 0
+//     }
+//     if(nums.length == 2){
+//         if(nums[0]> nums[1]){
+//             return 0
+//         }
+//         else{
+//             return 1
+//         }
+//     }
+//     if(nums[0] > nums[1]){
+//         return 0
+//     }
+//     else if(nums[nums.length-1]> nums[nums.length-2]){
+//         return nums.length-1
+//     }
+//     for(let i = 1; i<nums.length ; i++){
+//         if(nums[i] > nums[i-1] && nums[i]> nums[i+1]){
+//             return i
+//         }
+//     }
+//     return -1
