7d624d1fa57901be6fd82e9f2ad6dedad599c1d5
Ashish Varyani <ashuvaryani123@gmail.com>
2024-12-30 23:54:34 +0530

    Time: 0 ms (100.00%) | Memory: 49.1 MB (63.25%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
@@ -11,39 +11,17 @@
  * @return {number[]}
  */
 var inorderTraversal = function(root) {
-
-       if (root === null) return [];
-
-        const stack = [];
-        const result = [];
-
-     let currentNode = root;
-            while (stack.length > 0 || currentNode !== null) {
-            while (currentNode !== null) {
-                stack.push(currentNode);
-                currentNode = currentNode.left;
-            }
-
-            currentNode = stack.pop();
-            result.push(currentNode.val);
-            currentNode = currentNode.right;
-            }
-            return result
-}
-
-
-//  let result = []
-//      function helper(root){
-//          if(root !=null){
-//             if(root.left !=null){
-//                 helper(root.left)
-//             }
-//              result.push(root.val)
-//              if(root.right !=null){
-//                 helper(root.right)
-//             }
-
-//          }
-//      }
-//      helper(root)
-//      return result
+    let result = []
+    let stack = []
+    let currentNode = root
+    while(stack.length > 0 || currentNode !=null){
+        while(currentNode !=null){
+            stack.push(currentNode)
+            currentNode = currentNode.left
+        }
+       currentNode = stack.pop();
+       result.push(currentNode.val)
+        currentNode = currentNode.right
+    }
+    return result
+};
