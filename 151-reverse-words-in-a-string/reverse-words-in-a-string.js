645425cd6e4137db1869a19996ca88c4099118b8
Ashish Varyani <ashuvaryani123@gmail.com>
2025-01-07 14:56:27 +0530

    Time: 3 ms (49.69%) | Memory: 50.7 MB (70.36%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.js
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.js
@@ -3,21 +3,34 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    let newStr = s.trimStart()
-    let updtadedStr = newStr.trimEnd()
+     let newStr = s.trimStart()
+     let updtadedStr = newStr.trimEnd()
 
-    let arr= updtadedStr.split(" ")
-   
-    let newarr = []
-    for(let i=0; i<arr.length; i++){
-        // let temp1 = arr[i].trimStart()
-        // let temp2 = arr[i].trimEnd()
-         if(arr[i]){
-             newarr.push(arr[i])
-         }
-       
-    }
+      
+     let words= updtadedStr.split(" ")
 
-    let updatedArr= newarr.reverse().join(" ")
-    return updatedArr
+     let filterwords = words.filter((word)=> word != '')
+    
+     let reversewords = filterwords.reverse()
+
+     return reversewords.join(' ')
+
 };
+
+
+
+// let newStr = s.trimStart()
+//     let updtadedStr = newStr.trimEnd()
+
+//     let arr= updtadedStr.split(" ")
+   
+//     let newarr = []
+//     for(let i=0; i<arr.length; i++){
+//          if(arr[i]){
+//              newarr.push(arr[i])
+//          }
+       
+//     }
+
+//     let updatedArr= newarr.reverse().join(" ")
+//     return updatedArr
