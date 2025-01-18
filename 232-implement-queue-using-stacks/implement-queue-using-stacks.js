a9b2d4163a04bb99fcff2e20b99d6cc7120a281c
Ashish Varyani <ashuvaryani123@gmail.com>
2025-01-18 13:07:48 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (85.68%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.js
@@ -0,0 +1,70 @@
+
+var MyQueue = function() {
+    this.stack1 = [];  // Stack for pushing elements
+    this.stack2 = [];  // // Stack for popping elements
+
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+       this.stack1.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+      if (this.stack2.length === 0) {
+        while (this.stack1.length > 0) {
+            this.stack2.push(this.stack1.pop());
+        }
+    }
+    
+        // If stack2 is empty, the queue is empty
+        if (this.stack2.length === 0) {
+            return null; // Or handle as an error (queue is empty)
+        }
+
+        // Pop the front element from stack2
+        return this.stack2.pop();
+
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+     if (this.stack2.length === 0) {
+        while (this.stack1.length > 0) {
+            this.stack2.push(this.stack1.pop());
+        }
+    }
+
+    // If stack2 is empty, the queue is empty
+    if (this.stack2.length === 0) {
+        return null; // Or handle as an error (queue is empty)
+    }
+
+    // Return the front element from stack2
+    return this.stack2[this.stack2.length - 1];
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+     
+     return this.stack1.length === 0 && this.stack2.length === 0;
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
