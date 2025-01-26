7ee9dbf2ea38bd3e7b2f0abf92d0edea525db488
Ashish Varyani <ashuvaryani123@gmail.com>
2025-01-26 11:42:09 +0530

    Time: 62 ms (66.00%) | Memory: 66.8 MB (41.14%) - LeetSync

---
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
@@ -0,0 +1,81 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+
+ class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  getParentIndex(i) { return Math.floor((i - 1) / 2); }
+  getLeftChildIndex(i) { return 2 * i + 1; }
+  getRightChildIndex(i) { return 2 * i + 2; }
+
+  swap(i, j) {
+    [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+  }
+
+  insert(value) {
+    this.heap.push(value);
+    this.heapifyUp();
+  }
+
+  heapifyUp() {
+    let index = this.heap.length - 1;
+    while (this.getParentIndex(index) >= 0 &&
+           this.heap[this.getParentIndex(index)] > this.heap[index]) {
+      this.swap(this.getParentIndex(index), index);
+      index = this.getParentIndex(index);
+    }
+  }
+
+  removeMin() {
+    if (this.heap.length === 1) return this.heap.pop();
+
+    const min = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.heapifyDown();
+    return min;
+  }
+
+  heapifyDown() {
+    let index = 0;
+    while (this.getLeftChildIndex(index) < this.heap.length) {
+      let smallerChildIndex = this.getLeftChildIndex(index);
+      if (this.getRightChildIndex(index) < this.heap.length &&
+          this.heap[this.getRightChildIndex(index)] < this.heap[smallerChildIndex]) {
+        smallerChildIndex = this.getRightChildIndex(index);
+      }
+
+      if (this.heap[index] <= this.heap[smallerChildIndex]) break;
+
+      this.swap(index, smallerChildIndex);
+      index = smallerChildIndex;
+    }
+  }
+
+  size() {
+    return this.heap.length;
+  }
+
+  peek() {
+    return this.heap[0];
+  }
+}
+var findKthLargest = function(nums, k) {
+      const minHeap = new MinHeap();
+
+    for (const num of nums) {
+        minHeap.insert(num);
+
+        // Keep the heap size at most k
+        if (minHeap.size() > k) {
+        minHeap.removeMin();
+        }
+    }
+
+    // The root of the heap is the kth largest element
+    return minHeap.peek();
+};
