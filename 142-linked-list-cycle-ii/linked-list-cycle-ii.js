4d46b2f41ec09e9efb2b315f79db37701b961cab
Ashish Varyani <ashuvaryani123@gmail.com>
2025-02-20 17:57:14 +0530

    Time: 32 ms (100.00%) | Memory: 57.8 MB (5.95%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.js
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.js
@@ -11,25 +11,23 @@
  * @return {ListNode}
  */
 var detectCycle = function(head) {
-    if(!head || !head.next){
-        return null
-    }
-    let slow = head
-    let fast = head
+     let slow = head
+     let fast = head
 
-    while(fast && fast.next){
+     while( fast && fast.next !=null ){
         slow = slow.next
         fast = fast.next.next
         if(slow == fast){
-            let pointer1 = head
-            let pointer2 = slow
-
-            while(pointer1 != pointer2){
-                pointer1= pointer1.next
-                pointer2 = pointer2.next
-            }
-            return pointer1
+            slow = head
+        
+        while(slow !== fast){
+            slow = slow.next
+            fast= fast.next
         }
-    }
+
+        return fast
+     }
+     }
+
     return null
 };
